59f9fa8de3cad3954149f71d1c89d5bbcab6df18
yorbenGoor123 <yorbengoor5@gmail.com>
2020-06-15 12:03:22 +0200

    styling popup

--- src/app/content/apply/containers/personal/personal.js
+++ src/app/content/apply/containers/personal/personal.js
@@ -5,13 +5,23 @@ import FullNameIcon from "../../../../../assets/icons/name.svg"
 import EmailIcon from "../../../../../assets/icons/email.svg"
 import questionMark from "../../../../../assets/icons/popup.svg"
 import identityImage from "../../../../../assets/icons/identity.svg"
+import PopUpImage from "../../../../../assets/apply/identityPopup.svg"
+
+import Popup from "../../../../components/popup/popup"
 
 
 
 function Personal() {
+
+  const handlePopupClick = () => {
+    //popup moet verschijnen als je klikt op het vraagteken
+  }
+
   return (
   <>
+{/*<Popup title="What is the identification number (ID)" text="your national identification number (national ID-number, personal code or ID-code) is a unique 11 digit number assigned by the state and used for identification purposes in all Baltic Countries" image={PopUpImage} />*/}
   <div>
+
     <div>
       <p> Full Name </p> 
         <Field icon={FullNameIcon} value="Full name"/>
@@ -24,7 +34,7 @@ function Personal() {
     <div className={style.formField__identification}>
       <p> Identification Number (ID) </p> 
       <Field value="Identification Number" icon={identityImage}/>
-      <img className={style.button__popup} src={questionMark} alt="popup"></img>
+      <img onClick={handlePopupClick} className={style.button__popup} src={questionMark} alt="popup"></img>
     </div> 
   <div>
     <p> Age </p> 
