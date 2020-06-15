7880337e7c635ee9d93a707bcf7ee7932ff06bf8
yorbenGoor123 <yorbengoor5@gmail.com>
2020-06-15 11:11:50 +0200

    popup styling personal

--- src/app/components/field/field.js
+++ src/app/components/field/field.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from "./field.module.css"
 
 
-function Field({value,icon,width,validation}) {
+function Field({value,icon,width,validation, popup}) {
   return (
   <>
   <input className = {style.input} type = {(value === "Password" ? "password" : "text")} placeholder = {value} style = {{backgroundImage: `url(${icon})`}}/> 
--- src/app/content/apply/containers/personal/personal.js
+++ src/app/content/apply/containers/personal/personal.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import style from './personal.module.css';
 import Field from "../../../../components/field/field";
+import FullNameIcon from "../../../../../assets/icons/name.svg"
+import EmailIcon from "../../../../../assets/icons/email.svg"
+import questionMark from "../../../../../assets/icons/popup.svg"
 
 
 
@@ -8,26 +11,26 @@ function Personal() {
   return (
   <>
   <div>
-
+    <div>
+      <p> Full Name </p> 
+        <Field icon={FullNameIcon} value="Full name"/>
+     </div>  
+    <div>
+      <p> Email </p> 
+      <Field icon={EmailIcon} value="E-mail"/>
+    </div> 
+  <div className={style.identificationNumberAge__wrapper}>
+    <div className={style.formField__identification}>
+      <p> Identification Number (ID) </p> 
+      <Field value="Identification Number" popup={questionMark}/>
+      <img className={style.button__popup} src={questionMark} alt="popup"></img>
+    </div> 
   <div>
-  <p> Full Name </p> 
-  <Field/>
-  </div>  
-  <div>
-  <p> Email </p> 
-  <Field/>
-  </div> 
-  <div>
-  <div>
-  <p> Identification Number (ID) </p> 
-  <Field/>
-  </div> 
-  <div>
-  <p> Age </p> 
-  <Field/>
+    <p> Age </p> 
+    <Field value="Age"/>
   </div>
-  </div>
-  </div>
+</div>
+</div>
   </>
   );
 }
