0ac20df08a1fce5cf61f8aa6856f5b71a2756a4b
CDulst <casey.dulst@gmail.com>
2020-06-15 20:41:07 +0200

    fix

--- src/app/content/sidebar/containers/navItem/navItem.js
+++ src/app/content/sidebar/containers/navItem/navItem.js
@@ -2,12 +2,12 @@ import React from 'react';
 import style from './navItem.module.css';
 import {Link} from 'react-router-dom';
 
-function NavItem({text,icon,link}) {
+function NavItem({text,icon,link,active}) {
 return(
 <>
 <Link className = {style.link} to={link}>
 <div className = {style.wrapper}>
-<img className = {style.image} src = {icon} alt = "icon"/>
+<img className = {( active ? `${style.image} ${style.active}` : `${style.image}`)} src = {icon} alt = "icon"/>
 <p className = {style.text}> {text} </p>
 </div>
 </Link>
--- src/app/content/sidebar/index.js
+++ src/app/content/sidebar/index.js
@@ -6,6 +6,7 @@ import Carrier from "../../../assets/navbar/carrier.svg";
 import Challenge from "../../../assets/navbar/challenge.svg";
 import Placeholder from "../../../assets/navbar/placeholder.svg";
 import Tracker from "../../../assets/navbar/tracker.svg";
+import TrackerActive from "../../../assets/navbar/trackerSelected.png"
 import style from "./index.module.css";
 import {useEffect} from 'react';
 
@@ -17,7 +18,7 @@ function Sidebar() {
   <div className = {style.container}>
   <div className = {style.wrapper}>
   <div className = {style.navWrapper}>
-  <NavItem text = "Tracking" icon = {Tracker} link = {ROUTES.tracker}/>
+  <NavItem text = "Tracking" icon = {Tracker} link = {ROUTES.tracker} active = "true"/>
   <NavItem text = "Challenge" icon = {Challenge} link = {ROUTES.challenge}/>
   <NavItem text = "Carrier" icon = {Carrier} link = {ROUTES.carriers}/>
   </div>
