416148935cd37c9803ddd639c4083c1d6f4479c9
yorbenGoor123 <yorbengoor5@gmail.com>
2020-06-17 23:45:06 +0200

    map is in place

--- src/app/content/carriers/containers/discover/discover.js
+++ src/app/content/carriers/containers/discover/discover.js
@@ -10,25 +10,32 @@ const Map = ReactMapboxGl({
       minZoom: [2.7]
   });
 
+
+
 const Discover = ({start, end, type}) => {
+
+
+
+
     const [viewport] = useState({
-        containerStyle:{
-            height: '100vh',
-            width: '40vw'
-                },
+            container: 'map',
             zoom: [4],
             pitch:[60]
           
         })
 
+
+
+
     return (
-        <div className={style.mapboxContainer}>
+        <div id='map' className={style.mapboxContainer}>
             <div className={style.mapboxButton}>
             <Button value="discover journey"/>
             </div>
 
-
-            <Map {...viewport} style="mapbox://styles/yorbengoor/ckb6nfdnm3x4o1ip6nvt5psbb"/>
+            
+            <Map className={style.mapbox2} {...viewport} style="mapbox://styles/yorbengoor/ckb6nfdnm3x4o1ip6nvt5psbb"/>
+            
             
         </div>
     );
