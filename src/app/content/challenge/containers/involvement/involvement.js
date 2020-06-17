a5e75f479cd0475a7670f00d8f3bfdd9f0ea5540
CDulst <casey.dulst@gmail.com>
2020-06-17 16:09:52 +0200

    fixn

--- src/app/content/challenge/containers/involvement/involvement.js
+++ src/app/content/challenge/containers/involvement/involvement.js
@@ -11,7 +11,7 @@ function Involvement() {
     <div className={style.involvement__container}>
       <img src={involvementFlower} className={style.involvementPhoto} alt="involvement flower"></img>
       
-        <span className={style.counter}>$11199.8</span>
+        <span className={style.counter}>$11199.88</span>
       
     </div>
   </>
