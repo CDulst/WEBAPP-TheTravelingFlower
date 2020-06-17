5e1226f4add276b404e386f70e266c861c355ddf
CDulst <casey.dulst@gmail.com>
2020-06-17 13:45:20 +0200

    header styling

--- src/app/content/challenge/containers/header/header.js
+++ src/app/content/challenge/containers/header/header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import icon from "../../../../../assets/challenge/icon.svg"
+import question from "../../../../../assets/challenge/question.png"
 import style from './header.module.css'
 
 
 function Header({hashtags}) {
   return (
   <>
+    <img className = {style.question} src = {question} alt = "question"/>
   <div className={style.header}>
-  <img src = {icon} alt = "icon"/>
+  <img className = {style.image} src = {icon} alt = "icon"/>
   <div>
-  <h3> Location challenge </h3>
-  <p> USE <span>{hashtags}</span></p>
+  <h3 className = {style.title}> Today's Challenge </h3>
+  <p className = {style.para}> USE <span className = {style.hashtags}>{hashtags}</span></p>
   </div>
   </div>
   </>
