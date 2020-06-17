6b767852e0b5d41d1dca4be3651ff10edb7b235f
yorbenGoor123 <yorbengoor5@gmail.com>
2020-06-17 13:28:53 +0200

    video responsive

--- src/app/content/challenge/containers/assignment/assignment.js
+++ src/app/content/challenge/containers/assignment/assignment.js
@@ -12,11 +12,11 @@ function Assignment() {
   return (
   <>
   <div className={style.assignment__container}>
-  <img src = {line} alt = "line"/>
+
   <SwitchButton item1 = "text" item2 = "video"/>
   <ChallengeText icon = {icon} title = "Today the flower is located in Amsterdam" flag ={flag} text= "For today’s challenge, you need to make a Tulip in paper. Here is a good tutorial that can help: WWW.YOUTUBE.COM . Take a picture of your result and post in on your social media platforms. Each post is worth €0.20 cent that will be donated to the money jar. "/>
   <ChallengeVideo video = "/videos/video.mp4"/>
-  <img src = {line} alt = "line"/>
+ 
   </div>
   </>
   );
