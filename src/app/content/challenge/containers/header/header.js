d2f2d69ab9ececf1a425fde75a019e23075edb1b
CDulst <casey.dulst@gmail.com>
2020-06-17 11:06:08 +0200

    challenge containers jsx

--- src/app/content/challenge/containers/header/header.js
+++ src/app/content/challenge/containers/header/header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import icon from "../../../../../assets/challenge/icon.svg"
 
 
-
-function Header() {
+function Header({hashtags}) {
   return (
   <>
-
+  <div>
+  <img src = {icon} alt = "icon"/>
+  <div>
+  <h3> Location challenge </h3>
+  <p> USE <span>{hashtags}</span></p>
+  </div>
+  </div>
   </>
   );
 }
--- src/app/content/challenge/containers/socialfeed/socialfeed.js
+++ src/app/content/challenge/containers/socialfeed/socialfeed.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-
+import close from "../../../../../assets/feed/close.svg"
+import FeedHeader from "../../components/feedHeader/feedHeader";
+import Post from "../../components/post/post"
+import SocialExample from "../../../../../assets/feed/post/social symbols/twitter.svg"
+import ProfileExample from "../../../../../assets/feed/post/profile pics/example.svg"
+import ContentExample from "../../../../../assets/feed/post/content pics/example.svg"
 
 
 function SocialFeed() {
   return (
   <>
-
+  <div>
+  <img src = {close} alt = "close"/>
+  <FeedHeader hashtags = "#TravelingFlower #FlowerAmsterdamTF"/>
+  <Post pic = {ProfileExample} user = "Johnny Bravo" social = {SocialExample} text = "Ik ben een chad" content = {ContentExample} />
+  </div>
   </>
   );
 }
