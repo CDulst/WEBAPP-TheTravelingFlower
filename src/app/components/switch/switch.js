dc8ed09e26c4c0928165537fa595b0425259e794
yorbenGoor123 <yorbengoor5@gmail.com>
2020-06-17 09:48:21 +0200

    bug fixes container

---
+++ src/app/components/switch/switch.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './switch.module.css'
+
+const Switch = ({item1, item2, onchange}) => {
+
+    onchange = () => {
+
+    }
+
+    return (
+        <div className={style.switch__conainer}>
+            <p>hey</p>
+        </div>
+    );
+};
+
+export default Switch
