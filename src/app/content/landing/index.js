210fa803c4d66558018d48ac52684f2c3aa09f2c
CDulst <casey.dulst@gmail.com>
2020-06-19 17:42:32 +0200

    fix

--- src/app/content/landing/index.js
+++ src/app/content/landing/index.js
@@ -28,7 +28,7 @@ function Landing() {
   <div className =  {( uiLanding.uiOut ? `${style.rightSide} ${style.out}` : !uiLanding.uiOut ? `${style.rightSide} ${style.in}` : style.rightSide )}>
   <p onClick = {e => handleClick(e)}><img className = {style.arrow} src = {arrow} alt="arrow"/></p>
   <Switch>
-  <Route path = "/join">
+  <Route path = "/build/join">
   <Register/>
   </Route>
   <Route path = "/">
