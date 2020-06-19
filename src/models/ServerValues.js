cc4c33d7cc4c32d5a6c99da5f3d891b1b2365b58
yorbenGoor123 <yorbengoor5@gmail.com>
2020-06-19 12:42:42 +0200

    final data model

--- src/models/ServerValues.js
+++ src/models/ServerValues.js
@@ -1,15 +1,19 @@
 import { decorate, observable } from "mobx";
 
 class ServerValues {
-    constructor({location, donation=0}){
+    constructor({location, donation=0, currentChallenge, currentCarrier}){
         this.location = location;
         this.donation = donation;
+        this.currentCarrier = currentCarrier;
+        this.currentChallenge = currentChallenge;
     }
 }
 
 decorate(ServerValues, {
     location: observable,
-    donation: observable
+    donation: observable,
+    currentCarrier: observable,
+    currentChallenge: observable
 })
 
 export default ServerValues;
