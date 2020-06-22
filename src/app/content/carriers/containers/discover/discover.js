05d1a457bfd24f7d488f4991f21fb546e63606a9
yorbenGoor123 <yorbengoor5@gmail.com>
2020-06-22 14:56:54 +0200

    bug fixes

--- src/app/content/carriers/containers/discover/discover.js
+++ src/app/content/carriers/containers/discover/discover.js
@@ -37,7 +37,7 @@ const Discover = ({start, end, type}) => {
             </div>
 
         
-<Map center={(uiStoreCarriers.selectedRoute ? ([uiStoreCarriers.selectedRoute.startCoordinate.Rc, uiStoreCarriers.selectedRoute.startCoordinate.Ac+1]): [13.08434, 52.51435])} className={style.mapbox2} {...viewport} style="mapbox://styles/yorbengoor/ckbqhut424xj51iqattj488n8">
+<Map center={(uiStoreCarriers.selectedRoute ? ([uiStoreCarriers.selectedRoute.startCoordinate.Rc, uiStoreCarriers.selectedRoute.startCoordinate.Ac+2]): [13.08434, 52.51435])} className={style.mapbox2} {...viewport} style="mapbox://styles/yorbengoor/ckbqhut424xj51iqattj488n8">
     {uiStoreCarriers.selectedRoute ? (
         <>
                 <Image id={"marker-icon"} url={"https://upload.wikimedia.org/wikipedia/commons/2/28/Marker76887687.png"}></Image>
