068280d1880f88c999df1226d483caa3ddba5127
CDulst <casey.dulst@gmail.com>
2020-06-22 22:27:00 +0200

    change

--- src/app/content/sidebar/index.js
+++ src/app/content/sidebar/index.js
@@ -10,6 +10,7 @@ import TrackerActive from "../../../assets/navbar/trackerSelected.svg"
 import ChallengeActive from "../../../assets/navbar/challengeSelected.svg"
 import CarrierActive from "../../../assets/navbar/carrierSelected.svg"
 import {useObserver} from 'mobx-react-lite';
+import {rootStore} from '../../../stores/index'
 import style from "./index.module.css";
 
 
@@ -20,13 +21,13 @@ function Sidebar() {
   <div className = {style.container}>
   <div className = {style.wrapper}>
   <div className = {(uiStore.activePage === "tracker" ? `${style.active} ${style.activeTR}` : uiStore.activePage === "challenge" ?  `${style.active} ${style.activeCH}`: uiStore.activePage === "carriers" ?  `${style.active} ${style.activeCA}` : `${style.active} ${style.activeAC}`)}>
-  <img className = {`${style.activeImage} ${style.activeImageCenter}`} src = {(uiStore.activePage === "tracker" ? `${TrackerActive}` : uiStore.activePage === "challenge" ?  `${ChallengeActive}` : uiStore.activePage === "carriers" ? `${CarrierActive}` : `${Placeholder}` )} alt = "hello"/>
+  <img className = {`${style.activeImage} ${style.activeImageCenter}`} src = {(uiStore.activePage === "tracker" ? `${TrackerActive}` : uiStore.activePage === "challenge" ?  `${ChallengeActive}` : uiStore.activePage === "carriers" ? `${CarrierActive}` : `${(rootStore.uiStore.currentUser ? rootStore.uiStore.currentUser.avatar : "")}` )} alt = "hello"/>
   </div>
   <div className = {`${style.navWrapper}`}>
   <NavItem text = "Tracking" icon = {Tracker} link = {ROUTES.tracker} active = "true"/>
   <NavItem text = "Challenge" icon = {Challenge} link = {ROUTES.challenge}/>
   <NavItem text = "Carrier" icon = {Carrier} link = {ROUTES.carriers}/>
-  <NavItem text = "Account" icon = {Placeholder} link = {ROUTES.settings}/>
+  <NavItem text = "Account" icon =  {(rootStore.uiStore.currentUser ? rootStore.uiStore.currentUser.avatar : "")} link = {ROUTES.settings}/>
   </div>
   </div>
   </div>
