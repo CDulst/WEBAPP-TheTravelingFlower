75803e41f4c3e3125a72e3ccd6c180650666c8d6
yorbenGoor123 <yorbengoor5@gmail.com>
2020-06-22 14:37:55 +0200

    coordinates

--- src/app/content/carriers/containers/discover/discover.js
+++ src/app/content/carriers/containers/discover/discover.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import style from './discover.module.css'
 import Button from '../../../../components/button/button';
 import {Link} from "react-router-dom";
-import ReactMapboxGl from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Feature, Image } from 'react-mapbox-gl';
 import { useObserver } from 'mobx-react-lite';
 import {uiStoreCarriers} from '../../stores/UiStore';
 
@@ -11,7 +11,7 @@ const Map = ReactMapboxGl({
     accessToken:
       'pk.eyJ1IjoieW9yYmVuZ29vciIsImEiOiJja2IwcGE1Mm4wNTBuMzJtaXIwMjU3aHc2In0.URiltHb9cC88qeRt2HcbBA',
       
-      minZoom: [2.7]
+      minZoom: [2.8]
   });
 
 
@@ -20,7 +20,7 @@ const Discover = ({start, end, type}) => {
 
     const [viewport] = useState({
             container: 'map',
-            zoom: [8],
+            zoom: [5],
             pitch:[60]
           
         })
@@ -37,7 +37,27 @@ const Discover = ({start, end, type}) => {
             </div>
 
         
-<Map center={(uiStoreCarriers.selectedRoute ? ([uiStoreCarriers.selectedRoute.startCoordinate.Rc, uiStoreCarriers.selectedRoute.startCoordinate.Ac]): [13.08434, 52.51435])} className={style.mapbox2} {...viewport} style="mapbox://styles/yorbengoor/ckb6nfdnm3x4o1ip6nvt5psbb"></Map>
+<Map center={(uiStoreCarriers.selectedRoute ? ([uiStoreCarriers.selectedRoute.startCoordinate.Rc, uiStoreCarriers.selectedRoute.startCoordinate.Ac]): [13.08434, 52.51435])} className={style.mapbox2} {...viewport} style="mapbox://styles/yorbengoor/ckb6nfdnm3x4o1ip6nvt5psbb">
+    {uiStoreCarriers.selectedRoute ? (
+        <>
+                <Image id={"marker-icon"} url={"https://upload.wikimedia.org/wikipedia/commons/2/28/Marker76887687.png"}></Image>
+                
+
+                <Layer id="marker" layout={{"icon-image": "marker-icon", "icon-size": 0.5, "icon-ignore-placement": true, "icon-offset": [0,-20] }}  id="jumpingCarrier">
+                    <Feature coordinates={[uiStoreCarriers.selectedRoute.startCoordinate.Rc, uiStoreCarriers.selectedRoute.startCoordinate.Ac]}></Feature>
+                </Layer>
+
+                <Layer id="marker" layout={{"icon-image": "marker-icon", "icon-size": 0.5, "icon-ignore-placement": true, "icon-offset": [0,-20] }}  id="jumpingCarrier2">
+                    <Feature coordinates={[uiStoreCarriers.selectedRoute.endCoordinate.Rc, uiStoreCarriers.selectedRoute.endCoordinate.Ac]}></Feature>
+                </Layer>
+
+                <Layer type="line" paint={{"line-color":"#104ccf", "line-width": 4, "line-opacity": 0.5}}>
+                                <Feature coordinates={[[uiStoreCarriers.selectedRoute.startCoordinate.Rc, uiStoreCarriers.selectedRoute.startCoordinate.Ac], [uiStoreCarriers.selectedRoute.endCoordinate.Rc, uiStoreCarriers.selectedRoute.endCoordinate.Ac]]} />
+                </Layer>
+
+        </>
+    ):null}
+</Map>
         
             
      
