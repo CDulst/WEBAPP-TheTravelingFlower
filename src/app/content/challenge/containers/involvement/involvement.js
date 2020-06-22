62832ea60514328fa8b9134e2e47aa8976d3b77a
CDulst <casey.dulst@gmail.com>
2020-06-22 14:16:49 +0200

    git push

--- src/app/content/challenge/containers/involvement/involvement.js
+++ src/app/content/challenge/containers/involvement/involvement.js
@@ -9,9 +9,9 @@ function Involvement() {
   <>
     <div className={style.involvement__container}>
       <img src={involvementFlower} className={style.involvementPhoto} alt="involvement flower"></img>
-      
         <span className={style.counter}>€{( rootStore.serverValuesStore.donationController.currentdonation !== "" ? rootStore.serverValuesStore.donationController.currentdonation.toFixed(2) : "")}</span>
-      
+  <p className = {(!rootStore.serverValuesStore.donationController.rightSum ? `${style.added} ${style.left} ${style.in}` : `${style.added} ${style.left} ${style.out}`)}> + € {(rootStore.serverValuesStore.donationController.changeStatic ? rootStore.serverValuesStore.donationController.changeStatic : " ")}</p>
+        <p className = {(!rootStore.serverValuesStore.donationController.leftSum ? `${style.added} ${style.right} ${style.in}` : `${style.added} ${style.right} ${style.out}`)}> + €{(rootStore.serverValuesStore.donationController.changeStatic ? rootStore.serverValuesStore.donationController.changeStatic: "")}</p>
     </div>
   </>
   ));
--- src/controllers/donationController.js
+++ src/controllers/donationController.js
@@ -5,17 +5,27 @@ import {observable,action,decorate,configure} from "mobx"
 class DonationController {
   constructor(root){
   this.currentdonation = "";
-  this.livedonationSum = "";
+  this.livedonationSum = ""
+  this.setswitch = true;
+  this.leftSum = false;
+  this.rightSum = false;
   this.toAddMoney = 0;
-  this.waitTime = 20;
+  this.changeStatic = 0;
+  this.enabled = false;
+  this.waitTime = 5;
+  this.changed = false;
   this.EnableCounter();
   }
  
   EnableCounter(){
     setTimeout(() => {
-      this.toAddMoney = (this.livedonationSum - this.currentdonation).toFixed(2);
+      this.changeToAdd((this.livedonationSum - this.currentdonation).toFixed(2))
       this.donationCounter(this.toAddMoney);
       }, this.waitTime);
+  }
+
+  changeToAdd(value){
+      this.toAddMoney = value;
   }
 
   changecurrent(current){
@@ -25,14 +35,54 @@ class DonationController {
   changelive(live){
       this.livedonationSum = live;
   }
+
+  setLeft(){
+  this.changeLeftSum(true);
+  this.changeStatic = this.toAddMoney;
+  setTimeout(() => {
+  this.changeLeftSum(false);
+    }, 2000);
+  }
+
+  setRight(){
+  this.changeRightSum(true);
+  this.changeStatic = this.toAddMoney;
+  console.log(this.rightSum);
+  setTimeout(() => {
+    this.changeRightSum(false);
+  }, 2000);
+  }
+
+  changeRightSum(value) {
+   this.rightSum = value;
+  }
+
+  changeLeftSum(value){
+   this.leftSum = value
+  }
   
+  setSwitch(){
+      if(this.setswitch){
+          this.setLeft();
+          this.setswitch = false;
+      }
+      else{
+        this.setRight();
+        this.setswitch = true;
+      }
+  }
   
   donationCounter(){
   if (this.toAddMoney > 0){
+    if (!this.enabled){
+    this.enabled = true
+    this.setSwitch();
+    }
     this.currentdonation += 0.01;
     console.log(this.currentdonation.toFixed(2));
   }
   else{
+    this.enabled = false;
     this.toAddMoney = 0;
   }
   this.EnableCounter();
@@ -42,8 +92,17 @@ class DonationController {
 }
 decorate(DonationController,{
 currentdonation: observable,
+changeStatic: observable,
 livedonationSum: observable,
+changeLeftSum: action,
+changeToAdd: action,
+changeRightSum: action,
+leftSum: observable,
+setRight: action,
+setLeft: action,
+rightSum: observable,
 donationCounter: action,
+setSwitch: action,
 toAddMoney: observable,
 addedMoney: observable,
 changecurrent: action,
