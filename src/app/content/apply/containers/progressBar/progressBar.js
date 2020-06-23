0429d06c4dc5305eaae3db378acd588fa82a139c
CDulst <casey.dulst@gmail.com>
2020-06-23 21:42:05 +0200

    push

--- src/app/content/apply/containers/progressBar/progressBar.js
+++ src/app/content/apply/containers/progressBar/progressBar.js
@@ -4,31 +4,73 @@ import lineInactive from '../../../../../assets/apply/lineInactive.png'
 import lineActive from '../../../../../assets/apply/lineActive.png'
 import activeStep from '../../../../../assets/apply/activeStep.svg'
 import inactiveStep from '../../../../../assets/apply/inactiveStep.svg'
-
+import doneStep from '../../../../../assets/apply/doneStep.png'
+import uiApply from "../../stores/uiStore";
 
 function ProgressBar({type}) {
   return (
   <>
   <div className = {style.progressWrapper}>
   <h2 className = {style.hidden}> ProgressBar </h2>
-  <img className = {style.line} src = {activeStep} alt="active step" />
+  <img className = {(uiApply.currentStep === "personal" ? `${style.active} ${style.activestep1}` : uiApply.currentStep === "adres" ? `${style.active } ${style.activestep2}` : uiApply.currentStep === "route" ? `${style.active } ${style.activestep3}` :  `${style.active } ${style.activestep4}`) } src = {activeStep} alt="active step" />
   <div className = {style.stepWrapper}>
-  <img  src = {inactiveStep} alt="inactive step" />
-  <p className = {style.step}> Step 1 </p>
+  {
+
+  <img  src = {doneStep} alt="inactive step" />
+
+}
+  <p className = {style.step} style = {(uiApply.currentStep != "personal" ? {color:"red"} : {fontWeight: "1000",color:"red"})}> Step 1 </p>
+
   </div>
-  <img className = {style.line} src = {lineActive} alt="line active" />
+  {
+  (uiApply.currentStep != "personal"  ?
+  <img className = {style.line} src = {lineActive} alt="inactive step" />
+  :
+  <img className = {style.line} src = {lineInactive} alt="inactive step" />
+  )
+}
   <div className = {style.stepWrapper}>
-  <img src = {inactiveStep} alt="inactive step two" />
+  {
+  (uiApply.currentStep != "personal"  ?
+  <img  src = {doneStep} alt="inactive step" />
+  :
+  <img  src = {inactiveStep} alt="inactive step" />
+  )
+}
   <p className = {style.step}> Step 2 </p>
   </div>
-  <img className = {style.line} src = {lineInactive} alt="line inactive" />
+  {
+  (uiApply.currentStep != "personal" && uiApply.currentStep != "adres"  ?
+  <img className = {style.line} src = {lineActive} alt="inactive step" />
+  :
+  <img className = {style.line} src = {lineInactive} alt="inactive step" />
+  )
+}
   <div className = {style.stepWrapper}>
-  <img src = {inactiveStep} alt="inactive step three" />
+  {
+  (uiApply.currentStep != "personal" && uiApply.currentStep != "adres"  ?
+  <img  src = {doneStep} alt="inactive step" />
+  :
+  <img  src = {inactiveStep} alt="inactive step" />
+  )
+}
   <p className = {style.step}> Step 3 </p>
   </div>
-  <img className = {style.line} src = {lineInactive} alt="lineInactive step two" />
+  {
+  (uiApply.currentStep != "personal" && uiApply.currentStep != "adres" && uiApply.currentStep != "route"   ?
+  <img className = {style.line} src = {lineActive} alt="inactive step" />
+  :
+  <img className = {style.line} src = {lineInactive} alt="inactive step" />
+  )
+}
   <div className = {style.stepWrapper}>
-  <img src = {inactiveStep} alt = "inactive step four" />
+  {
+  (uiApply.currentStep != "personal" && uiApply.currentStep != "adres" && uiApply.currentStep != "route"  ?
+  <img  src = {doneStep} alt="inactive step" />
+  :
+  <img  src = {inactiveStep} alt="inactive step" />
+  )
+}
   <p className = {style.step}> Step 4 </p>
   </div>
   </div>
--- src/models/Journey.js
+++ src/models/Journey.js
@@ -2,7 +2,7 @@ import{ decorate, observable} from "mobx"
 import {rootStore} from "../stores/index.js";
 
 class Journey {
-    constructor({id, location, title, images, text, carrierID,date}) {
+    constructor({id, location,sorted, title, images, text, carrierID,date}) {
         this.id = id;
         this.location = location;
         this.title = title;
@@ -10,6 +10,7 @@ class Journey {
         this.text = text;
         this.carrierID = carrierID;
         this.date = date;
+        this.sorted = sorted;
         rootStore.journeyStore.addJourney(this);
         rootStore.carrierStore.findCarrierById(this.carrierID).linkJourney(this);
 
@@ -24,7 +25,8 @@ const journeyConverter = {
       images:journey.images,
       location:journey.location,
       text: journey.text,
-      title: journey.title
+      title: journey.title,
+      sorted: journey.sorted
       };
     },
     fromFirestore: function(snapshot, options) {
@@ -34,6 +36,7 @@ const journeyConverter = {
         return new Journey({
         id: snapshot.id,
         location: data.location,
+        sorted: data.sorted,
         title: data.title,
         images: data.images,
         text: data.text,
